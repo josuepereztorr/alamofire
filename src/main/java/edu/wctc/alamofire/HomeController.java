fe6440740e196445f3c14c6999db47a80ed69415
Josue <josuepereztorr@gmail.com>
2023-02-22 19:11:04 -0600

    - Get Request Mapping for home page

--- src/main/java/edu/wctc/alamofire/HomeController.java
+++ src/main/java/edu/wctc/alamofire/HomeController.java
@@ -1,2 +1,14 @@
-package edu.wctc.alamofire;public class HomeController {
+package edu.wctc.alamofire;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping()
+    public String getHomePage() {
+        return "index";
+    }
+
 }
