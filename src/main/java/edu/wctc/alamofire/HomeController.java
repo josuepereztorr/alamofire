d802a5aa67ac241ae78172e0a570918b1bb9a434
Josue <josuepereztorr@gmail.com>
2023-02-22 20:27:37 -0600

    - added tutorials and resources pages.

--- src/main/java/edu/wctc/alamofire/HomeController.java
+++ src/main/java/edu/wctc/alamofire/HomeController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping()
+    @GetMapping("/")
     public String getHomePage() {
         return "index";
+    }
+
+    @GetMapping("/tutorials")
+    public String getTutorialsPage() {
+        return "/tutorials/tutorials";
     }
 
 }
